314754ae46e0c05d8a188b13758226755a77f413
divya <divyab979@gmail.com>
2023-12-10 18:16:56 +0530

    first commit

--- pages/about.js
+++ pages/about.js
@@ -5,9 +5,6 @@ const about = () => {
   return (
     <main className='flex min-h-screen flex-col bg-[#121212]'>
       <Navbar/>
-      {/* <div class="container mx-auto px-12 py-4">
-        <HeroSection />
-      </div> */}
       <About/>
     </main>
   )
